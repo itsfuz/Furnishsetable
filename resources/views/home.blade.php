886fbf0ff5c450876bf62c51c0cad79b692dfd54
A <70055599+thelonedoodle@users.noreply.github.com>
2022-01-14 00:42:54 +0700

    views balde

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.masters')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <br>
+            <br>
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+             <p>test</p>
+             <br>
+             <br>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/layouts/masters.blade.php
@@ -0,0 +1,34 @@
+<!doctype html>
+
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    {{-- CSS --}}
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="{{ URL::asset('/css/furnishsetable.css') }}" rel="stylesheet">
+    <title>Furnishsetable</title>
+</head>
+
+<body>
+    @include('layouts.header')
+    <div class="p-10">
+        <br>
+        @yield('content')
+        <br>
+    </div>
+    @include('layouts.footer')
+
+
+    {{-- JS --}}
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
+    <script src="https://kit.fontawesome.com/c0f7e988b7.js" crossorigin="anonymous"></script>
+</body>
+
+</html>
