9580625cae151bfc422aa9bbbc833f085d11302c
itsfuz <80732778+itsfuz@users.noreply.github.com>
2022-01-14 23:17:53 +0700

    edited migration files

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cart;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
     //
+
+    public function CartPage($id){
+
+        $CartItems = Cart::where('users_id', $id)->get();
+
+        $products = Product::all();
+
+        $TotalCost = 0;
+
+        foreach($CartItems as $item){
+
+            $TotalCost = $TotalCost + $item->subtotal;
+        }
+
+        return view('cart')
+        ->with('CartItems', $CartItems)
+        ->;
+
+    }
 }
