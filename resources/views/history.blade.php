21b2db0b6cf61341117df158dad45d4eb5289adb
A <70055599+thelonedoodle@users.noreply.github.com>
2022-01-16 00:09:08 +0700

    Update history.blade.php

--- resources/views/history.blade.php
+++ resources/views/history.blade.php
@@ -3,16 +3,31 @@
 
 @section('content')
 
-<div style="flex-direction:row; display:flex">
-    @foreach ($Transactions as $transaction)
+<div class="container"  style="align-items:center;  justify-content:center">
+    <h3 style="text-align: center"><b>Transaction History</b></h3>
+    <br>
+    <table  class="table table-bordered" style="text-align: center">
+        <thead class="thead-dark">
+            <tr>
+                <th scope="col">Transaction ID</th>
+                <th scope="col">Transaction Date</th>
+                <th scope="col">Payment Method</th>
+            </tr>
 
-    <div class="content"><a href="/receipt/{{$transaction->id}}"><h3>{{$transaction->id}}</h3></a></div>
-    <p>this is supposed to be spaced out</p>
-    <div><h3>{{$transaction->transaction_date}}</h3></div>
-    <p>this is supposed to be spaced out</p>
-    <div><h3>{{$transaction->payment_method}}</h3></div>
+        </thead>
+        <tbody>
+            @foreach ($Transactions as $transaction)
+            <tr>
+                <th><div class="content" style="color: orange"><a href="/receipt/{{$transaction->id}}">{{$transaction->id}}</a></th>
+                <th>{{$transaction->transaction_date}}</th>
+                <th>{{$transaction->payment_method}}</th>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
 
-@endforeach
+
+
 </div>
 
 @endsection
