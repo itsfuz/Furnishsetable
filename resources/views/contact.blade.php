5473c6c4b6c64c344171e31a4b796f9dfd424247
reginaldlay <reginaldlay777@gmail.com>
2022-01-15 15:26:43 +0700

    Update contact.blade.php

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <h1>INI page contact</h1>
+        <h1>esbfnjfbhsnh</h1>
     </div>
 </div>
 @endsection
