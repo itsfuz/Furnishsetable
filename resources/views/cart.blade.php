c75b65383a8b7bec1073f9ff1fd907d7eaede045
A <70055599+thelonedoodle@users.noreply.github.com>
2022-01-15 23:00:19 +0700

    Update cart.blade.php

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.masters')
 
 @section('content')
-<div class="col-md-auto">
+
     <div class="container" style="align-items:center">
         @if ($size == null)
             <div class="card-header" style="text-align: center">
@@ -11,7 +11,7 @@
                 <br><br>
                 <img src="{{asset('images/cart.png')}}" style="width: 250px"/>
                 <br>
-                <h3 style="color: rgb(255, 153, 0)">Your Shopping Cart is currently Empty :(</h3>
+                <h3 style="color: rgb(255, 153, 0)"><b> Your Shopping Cart is currently Empty :(</b></h3>
                 <br><br>
                 <div class="col-md-auto">
                     <a href="/products"> <button class="btn btn-warning" type="submit">Shop Now!</button></a>
@@ -19,44 +19,50 @@
                 <br><br>
             </div>
         @else
-        <table class="table" style="display:flex">
-            <tr>
-                <th scope="col">Image</th>
-                <th scope="col">Name</th>
-                <th scope="col">Price</th>
-                <th scope="col">Quantity</th>
-                <th scope="col">Total Price</th>
-                <th scope="col">Action</th>
-            </tr>
-                <tbody>
-                    @foreach ($CartItems as $item)
+        <h2 style="text-align: center"><b>Shopping Cart</b></h2>
+        <br>
+        <div class="card"  style="align-items: center">
+            <table class="table" style="text-align: center">
+                <thead>
                     <tr>
-                        <th scope="col"><img src="{{Storage::url($item->image)}}" alt="image" style="width:200px;"></th>
-                        <th scope="col">{{$item->name}}</th>
-                        <th scope="col">{{$item->price}}</th>
-                        <th scope="col">{{$item->quantity}}</th>
-                        <th scope="col">{{$item->subtotal}}</th>
-                        <th scope="col">
-                            <a href="/minusQuantity/{{$item->product_id}}"><button type="button">-</button></a>
-                            <a href="/addQuantity/{{$item->product_id}}"><button type="button">+</button></a>
-                        </th>
+                        <th scope="col">Image</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Price</th>
+                        <th scope="col">Quantity</th>
+                        <th scope="col">Total Price</th>
+                        <th scope="col">Action</th>
                     </tr>
-                </tbody>
-            @endforeach
-        </table>
+                </thead>
 
-        <div class="col" style="text-align: center; font-size:20px">
-            <h1>Total Price: {{$TotalCost}}</h1>
-                <div class="col-md-auto">
-                    <a href="/goToCheckout/{{auth()->user()->id}}"><button type="submit" class="btn btn-dark">Checkout</button></a>
-                </div>
+                    <tbody>
+                        @foreach ($CartItems as $item)
+                        <tr>
+                            <th><img src="{{Storage::url($item->image)}}" alt="image" style="width:200px;"></th>
+                            <td>{{$item->name}}</td>
+                            <td>{{$item->price}}</td>
+                            <td>{{$item->quantity}}</td>
+                            <td>{{$item->subtotal}}</td>
+                            <td>
+                                <a href="/minusQuantity/{{$item->product_id}}"><button type="button" class="btn btn-danger">-</button></a>
+                                <a href="/addQuantity/{{$item->product_id}}"><button type="button" class="btn btn-success">+</button></a>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+            </table>
+            <br>
+            <div class="col" style="text-align: center; font-size:20px">
+                <h3>Total Price: Rp. {{$TotalCost}}</h3>
+                    <div class="col-md-auto">
+                        <a href="/goToCheckout/{{auth()->user()->id}}"><button type="submit" class="btn btn-dark">Checkout</button></a>
+                    </div>
+            </div>
+
+            <br><br>
         </div>
 
         @endif
 
     </div>
-
-</div>
-
 
 @endsection
