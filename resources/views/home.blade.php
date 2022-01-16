ad1476929b104a9553523dd85452f305a825307e
A <70055599+thelonedoodle@users.noreply.github.com>
2022-01-16 12:22:02 +0700

    fixed the home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,54 +2,60 @@
 
 @section('content')
 
-<link rel="stylesheet" href="style.css">
+<div class="container" style="display: flex;justify-content:center">
 
-<div class="col-md-auto" style="margin-bottom: 50px">
-    <div class="card-header  text-center">
-        <h1 style="color:Tomato;"><b>On Sale!</b></h1>
-    </div>
-    
-    <div class="slider">
-        <div class="slides">
-            <!--button-->
-            <input type="radio" name="radio-button" id="radio1">
-            <input type="radio" name="radio-button" id="radio2">
-            <input type="radio" name="radio-button" id="radio3">
-            <input type="radio" name="radio-button" id="radio4">
+    <div class="col-md-auto">
+        <div class="card" style="width: 800px; height: 500px;" >
+            <div class="card-header  text-center">
+                <h1 style="color:Tomato;"><b>On Sale!</b></h1>
+            </div>
+            <div class="slider">
+                <div class="slides">
+                    <!--button-->
+                    <input type="radio" name="radio-button" id="radio1">
+                    <input type="radio" name="radio-button" id="radio2">
+                    <input type="radio" name="radio-button" id="radio3">
+                    <input type="radio" name="radio-button" id="radio4">
 
-            <!--image-->
-            <div class="slide first">
-                <img src="" alt="">
-            </div>
-            <div class="slide">
-                <img src="" alt="">
-            </div>
-            <div class="slide">
-                <img src="" alt="">
-            </div>
-            <div class="slide">
-                <img src="" alt="">
-            </div>
+                    <!--image-->
+                    <div class="slide first">
+                        <img src="{{asset('images/example1.jpg')}}" alt="">
+                    </div>
+                    <div class="slide">
+                        <img src="{{asset('images/example1.jpg')}}" alt="">
+                    </div>
+                    <div class="slide">
+                        <img src="{{asset('images/example1.jpg')}}" alt="">
+                    </div>
+                    <div class="slide">
+                        <img src="{{asset('images/example1.jpg')}}" alt="">
+                    </div>
 
-            <!--automatic nav-->
-            <div class="auto-nav">
-                <div class="auto-button1"></div>
-                <div class="auto-button2"></div>
-                <div class="auto-button3"></div>
-                <div class="auto-button4"></div>
+                    <!--automatic nav-->
+                    <div class="auto-nav">
+                        <div class="auto-button1"></div>
+                        <div class="auto-button2"></div>
+                        <div class="auto-button3"></div>
+                        <div class="auto-button4"></div>
+                    </div>
+                </div>
+
+                <!--manual nav-->
+                <div class="manual-nav">
+                    <label for="radio1" class="manual-button"></label>
+                    <label for="radio2" class="manual-button"></label>
+                    <label for="radio3" class="manual-button"></label>
+                    <label for="radio4" class="manual-button"></label>
+                </div>
+
             </div>
         </div>
 
-        <!--manual nav-->
-        <div class="manual-nav">
-            <label for="radio1" class="manual-button"></label>
-            <label for="radio2" class="manual-button"></label>
-            <label for="radio3" class="manual-button"></label>
-            <label for="radio4" class="manual-button"></label>
-        </div>
+
 
     </div>
 </div>
+
 
 <script type="text/javascript">
     var counter = 1;
@@ -62,10 +68,4 @@
         }
     }, 5000);
 </script>
-
-<div class="container">
-    <div class="row justify-content-center">
-        TEST
-    </div>
-</div>
 @endsection
